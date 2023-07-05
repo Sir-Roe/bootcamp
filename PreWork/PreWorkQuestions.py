034fcec804aa4696088b0a28ad3f5bd3d90d44a2
Logan M Roe <Lmproe27@gmail.com>
2023-07-05 09:41:48 -0500

    Update PreWorkQuestions.py

--- PreWork/PreWorkQuestions.py
+++ PreWork/PreWorkQuestions.py
@@ -23,22 +23,31 @@ print("\n----------------\nQuestion3\n----------------")
 #Question 3
 #Please write a Python function, max_num_in_list to return the max number of a given list. 
 # The first line of the #code has been defined as below.
-my_list=list(range(1,43))
 
 def max_num_in_list(a_list):
      return max(a_list)
 
-biggest_num = max_num_in_list(my_list)
+#my_list=list(range(1,43))
+#biggest_num = max_num_in_list(my_list)
+#print(biggest_num)
 
-print(biggest_num)
 #Question 4
-#Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by #100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+#Write a function to return if the given year is a leap year. 
+# A leap year is divisible by 4, but not divisible by #100, unless it is also divisible by 400. 
+# The return should be boolean Type (true/false).
 
-#    def is_leap_year(a_year):
-#        .....
+def is_leap_year(a_year):
+    if (a_year % 400 == 0) and (a_year % 100 == 0):
+        return True
+    elif (a_year % 4 == 0) and (a_year % 100 != 0):
+        return True
+    else:
+        return False
                 
 #Question 5
-#Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are #consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+#Write a function to check to see if all numbers in list are consecutive numbers.
+# For example, [2,3,4,5,6,7] are #consecutive numbers, 
+# but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
-#    def is_consecutive(a_list):
-#        .....
+
+
