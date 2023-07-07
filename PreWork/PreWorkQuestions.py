cf8b368f0a34f08e2e38ab86163f831ee802211c
Logan M Roe <Lmproe27@gmail.com>
2023-07-07 08:00:27 -0500

    Update PreWorkQuestions.py

--- PreWork/PreWorkQuestions.py
+++ PreWork/PreWorkQuestions.py
@@ -1,5 +1,3 @@
-
-print("----------------\nQuestion1\n----------------")
 #Question 1
 #Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
 #The first line of the code has #been defined as below.
@@ -9,7 +7,7 @@ def hello_name(user_name):
 
 hello_name('Logan')
 
-print("\n----------------\nQuestion2\n----------------")
+
 #Question 2
 #Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
 
@@ -19,7 +17,6 @@ def first_odds():
 first_odds()
 
 
-print("\n----------------\nQuestion3\n----------------")
 #Question 3
 #Please write a Python function, max_num_in_list to return the max number of a given list. 
 # The first line of the #code has been defined as below.
@@ -27,6 +24,7 @@ print("\n----------------\nQuestion3\n----------------")
 def max_num_in_list(a_list):
      return max(a_list)
 
+#this is my test below feel free to use it
 #my_list=list(range(1,43))
 #biggest_num = max_num_in_list(my_list)
 #print(biggest_num)
@@ -49,8 +47,33 @@ def is_leap_year(a_year):
 # For example, [2,3,4,5,6,7] are #consecutive numbers, 
 # but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
-#def is_consecutive(a_list):
 
-my_list = [2,3,4,5,6,7]
-for item, i in enumerate(my_list):
-    print(item, i)
+
+def is_consecutive(a_list):
+#variables for function, sort the list first
+#used last num to track value of prior int, initialized as 0
+    a_list.sort()
+    iscon = True
+    last_num = 0
+    for i, item in enumerate(a_list):
+        #if we are at the beginning we don't do anything but set the last_num var
+        if i == 0:
+            last_num = item
+            continue
+        elif item != (last_num)+1:  
+            iscon = False
+            break
+        else:
+            last_num = item
+            
+    return iscon
+
+#i think for this to be really safe we would obviously need
+#   a way to validate that list is infact full of integers
+#   but since this wasn't a requirement I did not bother to add
+
+
+#my test data to validate.
+#a_list = [1,2,4,5]
+#a_list = [2,4,3,5,6,7]
+#print(is_consecutive(a_list))
