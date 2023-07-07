b6806250325362cefeeb52ae4e7b7f467d56c534
Logan M Roe <Lmproe27@gmail.com>
2023-07-07 07:02:07 -0500

    Update PreWorkQuestions.py

--- PreWork/PreWorkQuestions.py
+++ PreWork/PreWorkQuestions.py
@@ -49,5 +49,8 @@ def is_leap_year(a_year):
 # For example, [2,3,4,5,6,7] are #consecutive numbers, 
 # but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
+#def is_consecutive(a_list):
 
-
+my_list = [2,3,4,5,6,7]
+for item, i in enumerate(my_list):
+    print(item, i)
