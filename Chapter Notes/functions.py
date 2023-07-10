bc7a0adccb63436ef521b827c9278b67c77bf31f
Logan M Roe <Lmproe27@gmail.com>
2023-07-10 16:18:33 -0500

    Init

--- Chapter Notes/functions.py
+++ Chapter Notes/functions.py
@@ -1,10 +1,1 @@
-
-
-def do_something(a_list):
-    my_str=''
-    for ran in a_list:
-        my_str+=ran[0]
-    return my_str
-al_char = ['jaz','al','fazahl','abu']
-
-print(do_something(al_char))
+Do stuff
